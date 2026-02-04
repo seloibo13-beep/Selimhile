07d2885b310a10bd7e078e9a78762bdf503d0f02
seloibo13-beep <seloibo13@gmail.com>
2026-02-04 17:17:08 +0300

    Update Main.java

--- src/main/java/com/selim/hile/Main.java
+++ src/main/java/com/selim/hile/Main.java
@@ -1,41 +1,18 @@
 package com.selim.hile;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.network.play.client.C02PacketUseEntity;
+import javax.swing.JOptionPane;
 import java.awt.Toolkit;
 
 public class Main {
-    private static Minecraft mc = Minecraft.getMinecraft();
-    public static double range = 4.2; // Vuruş mesafesi (Blok)
+    public static void main(String[] args) {
+        // Bu kısım jar dosyasını tıkladığında çalışır
+        System.out.println("Selim Hile Altyapisi Calisiyor!");
+        Toolkit.getDefaultToolkit().beep();
+        JOptionPane.showMessageDialog(null, "Hile Altyapisi Basariyla Kuruldu!\nKillaura Modulu Beklemede.", "SelimHile", 1);
+    }
 
-    // Bu fonksiyonu her oyun tick'inde (güncellemesinde) çağıracağız
+    // Bu fonksiyonu daha sonra Minecraft'a bağlayacağız
     public static void onUpdate() {
-        // Dünyadaki tüm varlıkları tara
-        for (Object obj : mc.theWorld.loadedEntityList) {
-            if (obj instanceof EntityPlayer) {
-                EntityPlayer target = (EntityPlayer) obj;
-
-                // Kendimize vurmayalım ve mesafe kontrolü yapalım
-                if (target != mc.thePlayer && mc.thePlayer.getDistanceToEntity(target) <= range) {
-                    if (target.isEntityAlive()) {
-                        
-                        // 1. Sinyal ver (Bip sesi)
-                        Toolkit.getDefaultToolkit().beep();
-
-                        // 2. Vuruş Paketini Sunucuya Gönder
-                        mc.thePlayer.sendQueue.addToSendQueue(
-                            new C02PacketUseEntity(target, C02PacketUseEntity.Action.ATTACK)
-                        );
-                        
-                        // 3. El sallama animasyonu (Görsel olarak vuruyormuş gibi durur)
-                        mc.thePlayer.swingItem();
-                        
-                        // Her tick başına 1 kişiye vur (Ban riskini azaltmak için)
-                        break; 
-                    }
-                }
-            }
-        }
+        System.out.println("Killaura Tarama yapiyor...");
     }
 }
