72f205751d21d64c772e1eeda2ee967fd90a127c
seloibo13-beep <seloibo13@gmail.com>
2026-02-04 17:22:38 +0300

    Update Main.java

--- src/main/java/com/selim/hile/Main.java
+++ src/main/java/com/selim/hile/Main.java
@@ -4,9 +4,4 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Selim Hile Altyapisi Calisiyor!");
     }
-    
-    // Minecraft baglantisi icin bos bir method
-    public static void onUpdate() {
-        // Ileride killaura buraya gelecek
-    }
 }
