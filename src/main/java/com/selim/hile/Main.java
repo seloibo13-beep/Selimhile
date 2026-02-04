3acb43bf0dc1ef1fc819a8507e37c36c5f9e0366
seloibo13-beep <seloibo13@gmail.com>
2026-02-04 17:21:09 +0300

    Update Main.java

--- src/main/java/com/selim/hile/Main.java
+++ src/main/java/com/selim/hile/Main.java
@@ -1,18 +1,12 @@
 package com.selim.hile;
 
-import javax.swing.JOptionPane;
-import java.awt.Toolkit;
-
 public class Main {
     public static void main(String[] args) {
-        // Bu kısım jar dosyasını tıkladığında çalışır
         System.out.println("Selim Hile Altyapisi Calisiyor!");
-        Toolkit.getDefaultToolkit().beep();
-        JOptionPane.showMessageDialog(null, "Hile Altyapisi Basariyla Kuruldu!\nKillaura Modulu Beklemede.", "SelimHile", 1);
     }
-
-    // Bu fonksiyonu daha sonra Minecraft'a bağlayacağız
+    
+    // Minecraft baglantisi icin bos bir method
     public static void onUpdate() {
-        System.out.println("Killaura Tarama yapiyor...");
+        // Ileride killaura buraya gelecek
     }
 }
